d9d4ac69fe5b522f5cfe05d5e8395270b24a7327
SoaresDevOps <ryan.codes.dev@gmail.com>
2026-07-13 01:10:52 -0300

    Change default port from 5000 to 5002

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ app = create_app()
 
 if __name__ == '__main__':
     # Usando eventlet para latência zero no WebSocket
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5002))
     socketio.run(app, host='0.0.0.0', port=port, debug=False)
